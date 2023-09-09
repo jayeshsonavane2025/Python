376f9cc1feddd5d1003383f4a3e554262337a775
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-09 07:26:44 +0530

    learning basics

--- method.py
+++ method.py
@@ -3,10 +3,14 @@ def add(a, b):
     #typecasting to int
     return int(a) + int(b)
 
+
 a = input("Enter a: ")
 b = input("Enter b: ")
 
 #calling method
 result = add(a, b)
+
+
+
 
 print("The addition of " + a + " and " + b + " is " + str(result))
